edb0565fa3eeb333322c2facc220673109879859
Joakim Erik Åkerman <joakim.akerman@hotmail.com>
2025-09-29 21:07:08 +0200

    Added Forgot password logic

---
+++ Services/Interfaces/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using AuthServices.Data.Dto;
+using AuthServices.Data.Entities;
+using AuthServices.Models;
+using Microsoft.AspNetCore.Identity.Data;
+
+namespace AuthServices.Services.Interfaces;
+
+public interface IUserService
+{
+    Task<bool> CreateUser(RegisterDto model);
+    Task<UserReturnData?> GetUserById(string id);
+    Task<IEnumerable<UserReturnData>> GetAllUsers();
+    Task<bool> DeleteUser(string id);
+    Task<bool> UpdateUser(UsersEntity entity);
+    Task<UsersEntity?> GetUserByEmail(string email);
+    Task<bool> UserExistsByEmailAsync(string email);
+    Task RequestPasswordReset(string email);
+    Task<bool> ChangeForgottenPassword(ResetPasswordRequest request);
+}
